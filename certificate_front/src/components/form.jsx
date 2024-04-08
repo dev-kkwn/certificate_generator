1f82b88bbc01460d1c77679ff1d574f947c71fbc
kavin-lefty <kavinkumarsksi@gmail.com>
2024-04-08 23:03:21 +0530

    KK

--- certificate_front/src/components/form.jsx
+++ certificate_front/src/components/form.jsx
@@ -1,7 +1,55 @@
+import { useState } from "react";
 export function Forms() {
-    return (
-        <>
-        
-        </>
-    )
+  const initialData = {
+    name: "",
+    Date: "",
+    Course_Name: "",
+  };
+
+  const handleChange = (e) => {
+    setFormData((prevstate) => ({
+      ...prevstate,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  const [formData, setFormData] = useState(initialData);
+
+  const submitForm = (event) => {
+    event.preventDefault();
+    console.log(formData);
+    setFormData(initialData);
+  };
+  return (
+    <>
+      <form action="" onSubmit={submitForm}>
+        <label htmlFor="name">name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          className="border border-green-600 p-5"
+          onChange={handleChange}
+        />
+        <br />
+        <label htmlFor="name">Date</label>
+        <input
+          type="text"
+          id="date"
+          name="date"
+          className="border border-green-600 p-5"
+          onChange={handleChange}
+        />
+        <br />
+        <label htmlFor="name">Course Name</label>
+        <input
+          type="text"
+          id="course_name"
+          name="course_name"
+          className="border border-green-600 p-5"
+          onChange={handleChange}
+        />
+        <br />
+      </form>
+    </>
+  );
 }
