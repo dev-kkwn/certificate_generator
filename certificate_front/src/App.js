18af9be6dd8d328aa2bcf59bdda76ae9581ed5e4
kavin-lefty <kavinkumarsksi@gmail.com>
2024-04-09 18:32:26 +0530

    kk

--- certificate_front/src/App.js
+++ certificate_front/src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import { Forms } from './components/form';
+import logo from "./logo.svg";
+import "./App.css";
+import { Forms } from "./components/form";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-   <Forms/>
+    <>
+      <Forms />
+      <Routes>
+        <Route path="certificate" exact element={<Certificate />} />
+      </Routes>
+    </>
   );
 }
 
