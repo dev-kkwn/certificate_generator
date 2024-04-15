321378300a1902ee3875a2929a0a2a01fee4dd04
kavin-lefty <kavinkumarsksi@gmail.com>
2024-04-15 11:42:51 +0530

    table

--- certificate_front/src/App.js
+++ certificate_front/src/App.js
@@ -3,14 +3,16 @@ import "./App.css";
 import { Forms } from "./components/form";
 import { Route, Routes } from "react-router-dom";
 import Certificate from "./components/certificate";
+import { Table } from "./components/table";
 
 function App() {
   return (
     <>
-      <Routes>
+      {/* <Routes>
         <Route path="/" exact element={<Forms />} />
         <Route path="/certificate/:id" exact element={<Certificate />} />
-      </Routes>
+      </Routes> */}
+      <Table/>
     </>
   );
 }
